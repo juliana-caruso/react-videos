ba2e662701ad2580731a3626078bfcba223640b6
Juliana Caruso <70953242+juliana-caruso@users.noreply.github.com>
2021-06-22 15:54:35 +0200

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyA_ah7Czflufv7Gg0tSx3k-zhdJn6bFzB4';
+const KEY = 'GET A KEY';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
